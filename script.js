3031b283683038418474ab29a21756447ea3a752
AndreaMozzone <andreamozzone88@gmail.com>
2022-08-08 01:37:44 -0300

    subo cambios

---
+++ script.js
@@ -0,0 +1,12 @@
+//const nombres = ['ana', 'javier', 'maru', 'luis'];
+
+//nombres.splice(index, cantidad) indice y cantidad de elemenos a eliminar o agregar
+
+//nombres.splice(2,0, 'jose'); // en la posicion 2 no elimino a nadie y agrego a jose 
+
+//nombres.splice(2, 1, 'jose'); //en la posicion 2 elimino a 1 (maru) y agrego a jose
+
+//nombres.splice(2, 2, 'jose', 'toto'); //en el indice 2 elimino 2 y reemplazo por jose y toto
+
+//nombres.splice(2, 2);  // en el indice 2 elimine a 2  // ana, javier 
+//console.log(nombres)
